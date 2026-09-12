ab801b7e8d0e088b8d253a55734df31e23a25756
zaky arslan <zaky.arslan2@gmail.com>
2026-09-12 21:27:39 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,7 +1,8 @@
-import createMiddleware from "next-intl/middleware";
-import { routing } from "./i18n/routing";
+import { NextResponse } from "next/server";
 
-export default createMiddleware(routing);
+export default function middleware() {
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
